1e8ec51e295a78e4410e796691d1a9ebd332441d
greycm mac <cm.greys@gmail.com>
2022-08-30 10:40:15 -0400

    prueba

---
+++ js/origen.js
@@ -0,0 +1,21 @@
+//Origenes de café//
+let brasil = 15
+let Colombia = 16
+let Peru = 14
+
+
+let origen = prompt('¿Que origen quieres preparar?')
+alert ("Has seleccionado café de " +origen)
+console.log(origen)
+
+let cantidad = parseInt(prompt('¿Cuantos ml de café quieres preparar?'))
+//'ml de café de cafe de ' +origen)
+alert ('Has seleccionado preparar ' +cantidad)
+console.log(cantidad)
+
+if (origen === brasil){
+   let gramos = cantidad/brasil
+
+}
+
+alert("Debes usar " +gramos)
